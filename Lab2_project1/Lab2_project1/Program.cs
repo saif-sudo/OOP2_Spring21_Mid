f654775ea9d7e92162270067edb03a2e33a15250
Saif Ali <mdalisaif4@gmail.com>
2021-02-07 12:01:11 +0600

    Lab 2 project 1

---
+++ Lab2_project1/Lab2_project1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab2_project1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 20;
+            for (int i = 1; i <= n; i++)
+            {
+                
+                if (i % 2 == 0)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+
+            
+        }
+    }
+}

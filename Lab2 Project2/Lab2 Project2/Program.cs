c107a54c39884305188ffca797226f696734d4cc
Saif Ali <mdalisaif4@gmail.com>
2021-02-07 12:25:30 +0600

    Lab2 Project 2

---
+++ Lab2 Project2/Lab2 Project2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab2_Project2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 100;
+            int sum = 0;
+            for (int i = 1; i <= 100; i++) {
+               sum = sum + i;
+                Console.WriteLine(sum);
+            }
+
+        }
+    }
+}

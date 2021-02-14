48935a5ece10c617ddd50cb18162b9a182a3f5fe
Saif Ali <mdalisaif4@gmail.com>
2021-02-14 15:29:16 +0600

    Duplicate array elements project

--- ArrayDuplicateElementCount/ArrayDuplicateElementCount/Program.cs
+++ ArrayDuplicateElementCount/ArrayDuplicateElementCount/Program.cs
@@ -12,7 +12,32 @@ namespace ArrayDuplicateElementCount
         {
             int[] arr1 = {10 , 11 , 35 , 10 , 66 , 66 , 66 , 5 , 2 , 35 };
 
+            int i , j , carr, count = 0;
 
+            for (i = 0; i < arr1.Length; i++) {
+                carr = arr1[i];
+
+                for (j = 0; j < arr1.Length; j++) {
+                    if (carr == arr1[j]) {
+                        count++;
+                        
+                        
+                    }
+
+                    
+
+                    
+
+                }
+
+               
+
+                    Console.WriteLine("Total duplicate of arr1[" + i + "] =" + arr1[i] + " is = " + count);
+                
+
+
+                count = 0;
+            }
         }
     }
 }

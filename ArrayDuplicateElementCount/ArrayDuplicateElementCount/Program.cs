63e4db8cf1d930c61f06af70d415507193eb2eba
Saif Ali <mdalisaif4@gmail.com>
2021-02-14 14:00:20 +0600

    commit for submit before project

---
+++ ArrayDuplicateElementCount/ArrayDuplicateElementCount/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArrayDuplicateElementCount
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr1 = {10 , 11 , 35 , 10 , 66 , 66 , 66 , 5 , 2 , 35 };
+
+
+        }
+    }
+}
